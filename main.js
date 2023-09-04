267118c65513b272fc5ef802b1a0386b89300a3f
vovibssnff <vbessonov20@mail.ru>
2023-09-04 18:44:12 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const yField = document.getElementById("y_field");
+
+document.addEventListener()
+
+
+
+
+
+
+
+
+// let xmlHttp = new XMLHttpRequest();
+// xmlHttp.open("GET", theUrl, true);
+//
+// function httpGetAsync(theUrl, callback)
+// {
+//
+//     xmlHttp.onreadystatechange = function() {
+//         if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+//             callback(xmlHttp.responseText);
+//     }
+//     // true for asynchronous
+//     xmlHttp.send(null);
+// }
+//
+// // Define a callback function to handle the response
+// function handleResponse(responseText) {
+//     // Process the response here
+//     console.log("Response received:", responseText);
+// }
+//
+// var url = "file:///D:/Univer/web/lab_1/index.php";
+// httpGetAsync(url, handleResponse);
