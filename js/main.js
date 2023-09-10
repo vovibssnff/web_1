b1a5d781bfd6a6ea8fc019188639afc9a3dd6b07
vovibssnff <vbessonov20@mail.ru>
2023-09-10 12:18:06 +0300

    validation added

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
 const yField = document.getElementById("y_field");
 
-document.addEventListener()
-
-
-
+yField.addEventListener("input", (event) => {
+    const yValue = parseInt(yField.value);
+    console.log("Y is " + yValue);
+    if (isNaN(yValue)||yValue.toString()==="") {
+        yField.setCustomValidity("Y must be a valid number");
+    } else {
+        if (yValue<-3||yValue>5) {
+            yField.setCustomValidity("Y value should be in range [-3, 5]")
+        } else {
+            yField.setCustomValidity("");
+        }
+    }
+});
 
 
 
@@ -29,5 +38,5 @@ document.addEventListener()
 //     console.log("Response received:", responseText);
 // }
 //
-// var url = "file:///D:/Univer/web/lab_1/index.php";
+// var url = "file:///D:/Univer/web/lab_1/index.html";
 // httpGetAsync(url, handleResponse);
